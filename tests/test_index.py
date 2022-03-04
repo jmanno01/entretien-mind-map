41faf87251db98815033c05c0ab0dd849edacc24
jmanno01 <jay_manno@hotmail.com>
2022-03-04 02:43:24 -0500

    modified /tests directory

--- tests/test_index.py
+++ tests/test_index.py
@@ -18,7 +18,7 @@ def test_main_route_status_code(client) -> None:
 @pytest.fixture
 def db_client_map():
     app = Flask(__name__)
-
+    
     app.config["TESTING"] = True
     app.testing = True
 
@@ -50,31 +50,28 @@ def test_mindmap(db_client_map) -> None:
     assert rv.json == result
 
 
-# @pytest.fixture
-# def db_client_leaf():
-#     app = Flask(__name__)
+@pytest.fixture
+def db_client_leaf():
+    app = Flask(__name__)
 
-#     app.config["TESTING"] = True
-#     app.testing = True
+    app.config["TESTING"] = True
+    app.testing = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
 
-#     # This creates an in-memory sqlite db
-#     # See https://martin-thoma.com/sql-connection-strings/
-#     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
-
-#     client = app.test_client()
-#     with app.app_context():
-#         db.create_all()
-#         child = Child(path="I", text="", mindmap_id=1)
-#         db.session.add(child)
-#         db.session.commit()
-#     yield client
+    client = app.test_client()
+    with app.app_context():
+        db.create_all()
+        child = Child(path="I", text="", mindmap_id=1)
+        db.session.add(child)
+        db.session.commit()
+    yield client
 
 
-# def test_leaf(db_client_leaf) -> None:
-#     rv = db_client_leaf.get("/leaves")
-#     result = {
-#     "Leaves": [
-#             "{'path': 'root/I', 'text': '', 'map_info': mindmap_id=1 | mindmap_name=test-map}"
-#         ]
-# }
-#     assert rv.json == result
+def test_leaf(db_client_leaf) -> None:
+    rv = db_client_leaf.get("/leaves")
+    result = {
+    "Leaves": [
+            "{'path': 'root/I', 'text': '', 'map_info': mindmap_id=1 | mindmap_name=test-map}"
+        ]
+}
+    assert rv.json == result
